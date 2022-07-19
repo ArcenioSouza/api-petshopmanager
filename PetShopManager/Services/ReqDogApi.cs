2ec69c9ffa3e697daadb7d1a78e95e69f0f089e9
aozi <aozi@gft.com>
2022-07-19 16:12:24 -0300

    feat: :sparkles: Adiciona requisição com api TheDogApi

---
+++ PetShopManager/Services/ReqDogApi.cs
@@ -0,0 +1,29 @@
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetShopManager.Services
+{
+    public class ReqDogApi
+    {
+        public async Task<string> GetInfoDogs(string raca)
+        {
+            //Configura a requisição
+            string url = $"https://api.thedogapi.com/v1/breeds/search?q={raca}";
+            var req = WebRequest.Create(url);
+            req.Method = "GET";
+            req.ContentType = "application/json";
+            req.Headers["x-api-key"] = "83056c37-0343-48e2-b201-8d5b05e14ae4";
+
+            //Configura a resposta
+            var response = (HttpWebResponse)req.GetResponse();
+            var dataStream = response.GetResponseStream();
+            var reader = new StreamReader(dataStream);            
+            return reader.ReadToEnd();
+        }        
+    }
+}
