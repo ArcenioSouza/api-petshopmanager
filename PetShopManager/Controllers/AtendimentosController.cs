f802f7c5f5144b7b97e81679de2b9636d4a3f633
aozi <aozi@gft.com>
2022-07-25 15:02:09 -0300

    refactor: :recycle: Altera nome das rotas para melhor organização e padronização

--- PetShopManager/Controllers/AtendimentosController.cs
+++ PetShopManager/Controllers/AtendimentosController.cs
@@ -13,7 +13,7 @@ using PetShopManager.Models;
 namespace PetShopManager.Controllers
 {
     [ApiController]
-    [Route("api/v1/[controller]")]
+    [Route("api/v1/atendimentos")]
     public class AtendimentosController : ControllerBase
     {
         private readonly ApplicationDbContext _database;
@@ -23,104 +23,11 @@ namespace PetShopManager.Controllers
         }
 
         [Authorize(Roles = "Funcionario")]
-        [HttpGet]
-        public async Task<ActionResult> Get()
-        {
-            try
-            {
-                List<Atendimento> ListaRegistrosDeAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
-
-                if (ListaRegistrosDeAtendimentos.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
-
-                return Ok(ListaRegistrosDeAtendimentos);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { erro = ex.Message });
-            }
-        }
-
-        [Authorize(Roles = "Cliente, Funcionario")]
-        [HttpGet("Cliente/BuscaPorId/{id}")]
-        public async Task<ActionResult> GetByIdCliente(int id)
-        {
-            try
-            {
-                List<Atendimento> ListaRegistrosAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Where(atendimento => atendimento.Cliente.Id == id).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
-
-                if (ListaRegistrosAtendimentos.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
-
-                return Ok(ListaRegistrosAtendimentos);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { erro = ex.Message });
-            }
-        }
-
-        [Authorize(Roles = "Cliente, Funcionario")]
-        [HttpGet("Cliente/Busca/{nome}/{cpf}")]
-        public async Task<ActionResult> GetByNomeCliente(string nome, string cpf)
-        {
-            try
-            {
-                Cliente ValidarCliente = _database.Clientes.First(cliente => cliente.Nome == nome);
-                if (ValidarCliente is null) return NotFound("Cliente não encontrado");
-                if (ValidarCliente.Cpf != cpf) return StatusCode(StatusCodes.Status401Unauthorized, "Informações do cliente não coincidem com o banco de dados");
-
-                List<Atendimento> ListaRegistrosAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Where(atendimento => atendimento.Cliente.Nome == nome).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
-
-                return Ok(ListaRegistrosAtendimentos);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { erro = ex.Message });
-            }
-        }
-
-        [Authorize(Roles = "Cliente, Funcionario")]
-        [HttpGet("Animal/BuscaPorId/{id}")]
-        public async Task<ActionResult> GetByIdAnimal(int id)
-        {
-            try
-            {
-                List<Atendimento> ListaRegistros = await _database.Atendimentos.Include(animal => animal.Animal).Where(animal => animal.Animal.Id == id).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
-
-                if (ListaRegistros.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
-
-                return Ok(ListaRegistros);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { erro = ex.Message });
-            }
-        }
-
-        [Authorize(Roles = "Cliente, Funcionario")]
-        [HttpGet("Animal/Busca/{Nome}/{DataNascimento}")]
-        public async Task<ActionResult> GetByNomeAnimal(string Nome, string DataNascimento)
-        {
-            try
-            {
-                var dataNascimento = Convert.ToDateTime(DataNascimento);
-
-                List<Atendimento> ListaRegistros = await _database.Atendimentos.Include(animal => animal.Animal).Where(animal => animal.Animal.Nome == Nome && animal.Animal.DataNascimento == dataNascimento).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
-
-                if (ListaRegistros.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
-
-                return Ok(ListaRegistros);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { erro = ex.Message });
-            }
-        }
-
-        [Authorize(Roles = "Funcionario")]
         [HttpPost]
         public async Task<ActionResult> Post(AtendimentoDTO atendimentoTemp)
         {
-            try{
+            try
+            {
                 Animal AnimalTemp = await _database.Animais.FirstAsync(animal => animal.Id == atendimentoTemp.AnimalId);
                 Funcionario FuncionarioTemp = await _database.Funcionarios.Include(cargo => cargo.Cargo).FirstAsync(funcionario => funcionario.Id == atendimentoTemp.FuncionarioId);
                 Servico ServicoTemp = await _database.Servicos.FirstAsync(servico => servico.Id == atendimentoTemp.ServicoId);
@@ -146,6 +53,100 @@ namespace PetShopManager.Controllers
                 await _database.SaveChangesAsync();
 
                 return Ok(AtendimentoParaSalvar);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Funcionario")]
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                List<Atendimento> ListaRegistrosDeAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
+
+                if (ListaRegistrosDeAtendimentos.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
+
+                return Ok(ListaRegistrosDeAtendimentos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Cliente, Funcionario")]
+        [HttpGet("cliente/busca/{id}")]
+        public async Task<ActionResult> GetByIdCliente(int id)
+        {
+            try
+            {
+                List<Atendimento> ListaRegistrosAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Where(atendimento => atendimento.Cliente.Id == id).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
+
+                if (ListaRegistrosAtendimentos.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
+
+                return Ok(ListaRegistrosAtendimentos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Cliente, Funcionario")]
+        [HttpGet("cliente/busca/{nome}/{cpf}")]
+        public async Task<ActionResult> GetByNomeCliente(string nome, string cpf)
+        {
+            try
+            {
+                Cliente ValidarCliente = _database.Clientes.First(cliente => cliente.Nome == nome);
+                if (ValidarCliente is null) return NotFound("Cliente não encontrado");
+                if (ValidarCliente.Cpf != cpf) return StatusCode(StatusCodes.Status401Unauthorized, "Informações do cliente não coincidem com o banco de dados");
+
+                List<Atendimento> ListaRegistrosAtendimentos = await _database.Atendimentos.Include(animal => animal.Animal).Include(cliente => cliente.Cliente).Where(atendimento => atendimento.Cliente.Nome == nome).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
+
+                return Ok(ListaRegistrosAtendimentos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Cliente, Funcionario")]
+        [HttpGet("animal/busca/{id}")]
+        public async Task<ActionResult> GetByIdAnimal(int id)
+        {
+            try
+            {
+                List<Atendimento> ListaRegistros = await _database.Atendimentos.Include(animal => animal.Animal).Where(animal => animal.Animal.Id == id).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
+
+                if (ListaRegistros.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
+
+                return Ok(ListaRegistros);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { erro = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Cliente, Funcionario")]
+        [HttpGet("animal/busca/{nome}/{nascimento}")]
+        public async Task<ActionResult> GetByNomeAnimal(string nome, string nascimento)
+        {
+            try
+            {
+                var dataNascimento = Convert.ToDateTime(nascimento);
+
+                List<Atendimento> ListaRegistros = await _database.Atendimentos.Include(animal => animal.Animal).Where(animal => animal.Animal.Nome == nome && animal.Animal.DataNascimento == dataNascimento).Include(cliente => cliente.Cliente).Include(funcionario => funcionario.Funcionario).Include(servico => servico.Servico).ToListAsync();
+
+                if (ListaRegistros.Count == 0) return NotFound("Nenhum registro de atendimento encontrado");
+
+                return Ok(ListaRegistros);
             }
             catch (Exception ex)
             {
